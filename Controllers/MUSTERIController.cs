0794ecbe6a91623f0b8f9d4bd8e9fea09b55b6bc
cengiztuglu <cngztuglu@gmail.com>
2022-10-09 22:11:12 +0300

    pull customer data

---
+++ Controllers/MUSTERIController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCSTOKTAKIP.Models.Entity;
+
+namespace MVCSTOKTAKIP.Controllers
+{
+    
+    public class MUSTERIController : Controller
+    {
+        // GET: MUSTERI
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLMUSTERILER.ToList();
+            return View(degerler);
+        }
+    }
+}
