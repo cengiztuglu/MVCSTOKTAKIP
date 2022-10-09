86811894d01cccbcd7b8384d99ff538f37b84b6c
cengiztuglu <cngztuglu@gmail.com>
2022-10-09 17:26:36 +0300

    Add project files.

---
+++ Controllers/KategoriController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCSTOKTAKIP.Models.Entity;
+
+namespace MVCSTOKTAKIP.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+      
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORILER.ToList();
+            return View(degerler);
+
+        }
+    }
+}
